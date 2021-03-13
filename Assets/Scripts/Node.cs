bc10accb22cb4fdaf60942e868d387effa5f44b2
OmarElSayed97 <omarihabelsayed@gmail.com>
2021-03-13 22:46:41 +0200

    Adding classes

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AStar5DVR
+{
+    public class Node 
+    {
+        #region Variables
+        public Node Parent;
+        public Vector2 Position;
+        public bool Walkable;
+        public float H;
+        public float G;
+        public float F
+        {
+            get
+            {
+                if (H != -1 && G != -1)
+                    return H + G;
+                else
+                    return -1;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public Node(Vector2 pos, bool walkable, float weight = 1)
+        {
+            Parent = null;
+            Position = pos;
+            H = -1;
+            G = 1;
+
+            Walkable = walkable;
+        }
+        #endregion
+
+    }
+}
+
